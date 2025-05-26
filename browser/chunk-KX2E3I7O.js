c09de390f8ba72377ce498e3a8ac23c4f6677d8f
siarheichura <78800419+siarheichura@users.noreply.github.com>
2025-05-26 16:56:47 +0000

    Deploying to gh-pages from @ siarheichura/potolki-admin@c6ca3cfca067554e616fbde84e585c7a995aae53 🚀

--- browser/chunk-KX2E3I7O.js
+++
@@ -1,1 +0,0 @@
-import{e as o,j as e}from"./chunk-ARRAJFJC.js";var r=o((n,t)=>{e();t.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default r();
---
+++ browser/chunk-P3PRAJQJ.js
@@ -0,0 +1,1 @@
+import{e as o,j as e}from"./chunk-EL4TFKRS.js";var r=o((n,t)=>{e();t.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}});export default r();
