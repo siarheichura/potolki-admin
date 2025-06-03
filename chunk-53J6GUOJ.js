ded225acd34b694002f46cfdd13caca15faa36d1
siarheichura <78800419+siarheichura@users.noreply.github.com>
2025-06-03 15:59:02 +0000

    Deploying to gh-pages from @ siarheichura/potolki-admin@6f31e90cbbf45ae0c74703b7c76e745663648947 🚀

--- chunk-53J6GUOJ.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c,d,e,g as f}from"./chunk-MNH42E2L.js";import"./chunk-EL4TFKRS.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
---
+++ chunk-WBQ7TBQD.js
@@ -0,0 +1,1 @@
+import{a,b,c,d,e,g as f}from"./chunk-M4XXU5DX.js";import"./chunk-ARRAJFJC.js";f();export{c as Headers,d as Request,e as Response,b as default,a as fetch};
